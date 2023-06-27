872d9e4f46bdec1603958c71899adddab09195d3
John Doe <johndoe@email.com>
2023-06-27 22:06:38 +0600

    6

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
+from io import BytesIO
 from typing import Final
 
 import openai
+import requests
 from dotenv import load_dotenv
-from telegram import Update
+from serpapi import GoogleSearch
+from telegram import InputMediaPhoto, Update
 from telegram.ext import (Application, CommandHandler, ContextTypes,
                           MessageHandler, filters)
 
@@ -13,6 +16,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+SERP_API_KEY = os.getenv("SERP_API_KEY")
 
 TOKEN: Final = BOT_TOKEN
 BOT_USERNAME: Final = '@freestyler1_bot'
@@ -38,6 +42,55 @@ async def setplace_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global setplace_cond
     setplace_cond = True
     await update.message.reply_text('Type some place with whose guide you want to talk with')
+
+
+async def pictures_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if place is not None:
+        params = {
+            "q": "pictures of " + place,
+            "engine": "google_images",
+            "ijn": "0",
+            "api_key": SERP_API_KEY
+        }
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        images = []
+        for i in range(3):
+            url = results["images_results"][i]['original']
+            response = requests.get(url)
+            if response.status_code == 200:
+                photo_file = BytesIO(response.content)
+                images.append(photo_file)
+            else:
+                images.append(results["images_results"][i]["thumbnail"])
+        media_group = [InputMediaPhoto(url) for url in images]
+        message_text = "Here are some pictures of " + place + "!"
+        await context.bot.send_message(chat_id=update.message.chat_id, text=message_text)
+        await context.bot.send_media_group(chat_id=update.message.chat_id, media=media_group)
+    else:
+        await update.message.reply_text('Type /setplace to choose a place before you ask pictures')
+
+
+async def weather_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if place is not None:
+        params = {
+            "q": "nyc weather",
+            "hl": "en",
+            "gl": "us",
+            "google_domain": "google.com",
+            "api_key": SERP_API_KEY
+        }
+
+        search = GoogleSearch(params)
+        results = search.get_dict()
+        temperature_fahrenheit = int(results["answer_box"]["temperature"])
+        temperature_celsius = int((temperature_fahrenheit - 32) / 1.8)
+        wind = results["answer_box"]["wind"]
+        weather = results["answer_box"]["weather"]
+        message = f"Temperature: {temperature_celsius} celsuis\nWind: {wind}\n{weather}"
+        await context.bot.send_message(chat_id=update.message.chat_id, text=message)
+    else:
+        await update.message.reply_text('Type /setplace to choose a place before you ask pictures')
 
 
 def handle_response(text):
@@ -102,6 +155,8 @@ if __name__ == '__main__':
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('help', help_command))
     app.add_handler(CommandHandler('setplace', setplace_command))
+    app.add_handler(CommandHandler('pictures', pictures_command))
+    app.add_handler(CommandHandler('weather', weather_command))
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
     app.add_error_handler(error)
     app.run_polling(poll_interval=5)
